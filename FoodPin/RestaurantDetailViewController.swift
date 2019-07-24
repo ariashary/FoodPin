863342c3a67f3eaa0ec7ce652d870efc21327039
Muhammad Ashary <muh.ariashary@gmail.com>
2019-07-24 13:11:22 +0700

    Add unwind segues and animation view

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -66,7 +66,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             cell.valueLabel.text = restaurant.phone
         case 4:
             cell.fieldLabel.text = "Been here"
-            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before" : "No"
+            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before. \(restaurant.rating)" : "No"
         default:
             cell.fieldLabel.text = ""
             cell.valueLabel.text = ""
@@ -75,6 +75,33 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         cell.backgroundColor = UIColor.clear
         
         return cell
+    }
+    
+    @IBAction func close(segue: UIStoryboardSegue) {
+        
+    }
+    
+    @IBAction func ratingButtonTapped(segue: UIStoryboardSegue) {
+        if let rating = segue.identifier {
+            restaurant.isVisited = true
+            
+            switch rating {
+            case "great": restaurant.rating = "Absolutely love it! Must try."
+            case "good": restaurant.rating = "Pretty good."
+            case "dislike": restaurant.rating = "I don't like it."
+            default: break
+            }
+        }
+        
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showReview" {
+            let destinationController = segue.destination as! ReviewViewController
+            
+            destinationController.restaurant = restaurant
+        }
     }
     
 }
---
+++ FoodPin/ReviewViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ReviewViewController.swift
+//  FoodPin
+//
+//  Created by Muhammad Ashary on 24/07/19.
+//  Copyright © 2019 M. Ashary. All rights reserved.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var reviewImageView: UIImageView!
+    @IBOutlet var buttonClose: UIButton!
+    
+    var restaurant: Restaurant?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let blureffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blureffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+        let scaleTransform = CGAffineTransform.init(scaleX: 0, y: 0)
+        let translateTransform = CGAffineTransform.init(translationX: 0, y: -1000)
+        let combineTransform = scaleTransform.concatenating(translateTransform)
+        containerView.transform = combineTransform
+        
+        if let restaurant = restaurant {
+            reviewImageView.image = UIImage(named: restaurant.image)
+        }
+        
+        buttonClose.transform = CGAffineTransform.init(translationX: 1000, y: 0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.transform = CGAffineTransform.identity
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: .curveEaseInOut, animations: {
+            
+            self.buttonClose.transform = CGAffineTransform.identity
+            
+        }, completion: nil)
+    }
+
+}
